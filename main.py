73c4e3085f331e453860608e5f1c139d65c156d2
yerimah <danielyerimah@yahoo.com>
2024-12-14 10:54:16 +0100

    First commit in python

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Addition 1: Add two numbers entered by the user")
+
+print("Step 1: Start")
+print("Step 2: Declare variables num1, num2, sum")
+print("Step 3: Read values for num1 and num2")
+print("Step 4: Add num1 and num2 and assign the result to sum")
+print("Step 5: Display sum")
+print("Step 6: Stop")
